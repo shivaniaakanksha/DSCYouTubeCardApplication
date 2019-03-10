1093a362c164e59195bab5d4815095cf90e25aa2
shivaniaakanksha <shivaniaakanksha@gmail.com>
2019-03-10 12:44:58 +0530

    Initial commit

---
+++ app/src/main/java/com/edu/kiet/dsc_youtube_card_application/informationActivity.java
@@ -0,0 +1,13 @@
+package com.edu.kiet.dsc_youtube_card_application;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class informationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information);
+    }
+}
