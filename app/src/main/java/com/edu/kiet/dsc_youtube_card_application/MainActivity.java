2fbb9a298395eadf782afe315d4f4a72f6b4e26d
shivaniaakanksha <shivaniaakanksha@gmail.com>
2019-03-11 17:21:26 +0530

    day 5

--- app/src/main/java/com/edu/kiet/dsc_youtube_card_application/MainActivity.java
+++ app/src/main/java/com/edu/kiet/dsc_youtube_card_application/MainActivity.java
@@ -2,12 +2,27 @@ package com.edu.kiet.dsc_youtube_card_application;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn1=findViewById(R.id.btn1);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),informationActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
 }
--- app/src/main/java/com/edu/kiet/dsc_youtube_card_application/informationActivity.java
+++ app/src/main/java/com/edu/kiet/dsc_youtube_card_application/informationActivity.java
@@ -2,12 +2,36 @@ package com.edu.kiet.dsc_youtube_card_application;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class informationActivity extends AppCompatActivity {
+
+    Button btn_changetext;
+    TextView tv_text;
+    EditText eInput;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_information);
+
+        tv_text=findViewById(R.id.change);
+        btn_changetext=findViewById(R.id.btn_changetext);
+        eInput =findViewById(R.id.input);
+
+        btn_changetext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String st=eInput.getText().toString();
+                tv_text.setText(st);
+                Toast.makeText(getApplicationContext(),"Successfully commented",Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
 }
